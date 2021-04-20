4d607c453655890b6463480d01ab38dbde2908b2
sirasatarato <sirasatarato@gmail.com>
2021-04-20 16:20:28 +0900

    Refactor Appbar to extract Widget

---
+++ incident_tracker_flutter/lib/src/widgets/incident_tacker_appbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:incident_tracker_flutter/src/utility/icons/write_post_icons.dart';
+
+class IncidentTrackerAppbar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      centerTitle: true,
+      title: Text(
+        'Incident Tracker',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 15,
+          fontFamily: 'Blanka',
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: writePost,
+          icon: Icon(
+            WritePost.edit,
+            color: Theme.of(context).accentColor,
+          ),
+        ),
+      ],
+    );
+  }
+
+  void writePost() {
+
+  }
+
+}
