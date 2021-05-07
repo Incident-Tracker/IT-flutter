c264d017739ac4b60f3d867a8ca0e764a6cff30b
sirasatarato <sirasatarato@gmail.com>
2021-05-07 19:13:23 +0900

    Delete CategoryBuilder and Add dummy data

--- incident_tracker_flutter/lib/src/pages/category_page.dart
+++ incident_tracker_flutter/lib/src/pages/category_page.dart
@@ -1,48 +1,21 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:incident_tracker_flutter/src/pages/result_page.dart';
+import 'package:incident_tracker_flutter/src/widgets/category_box.dart';
 
 class CategoryPage extends StatelessWidget {
+  List<Widget> list = [
+    CategoryBox('연애', Icons.live_tv_rounded),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.fromLTRB(16, 32, 16, 0),
+      padding: const EdgeInsets.fromLTRB(16, 32, 16, 0),
       child: GridView.count(
         crossAxisCount: 3,
         mainAxisSpacing: 14,
         crossAxisSpacing: 16,
-        children: List.generate(44, (index) => buildCategory(index)),
+        children: list,
       ),
-    );
-  }
-
-  Widget buildCategory(int index) {
-    return GestureDetector(
-      onTap: () => Get.to(ResultPage('Item $index')),
-      child: Container(
-            color: Color(Random().nextInt(0xffffffff)).withOpacity(0.4),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                SizedBox(),
-                Icon(
-                  Icons.videogame_asset,
-                  size: 48,
-                ),
-                Text(
-                  'Item $index',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: "NotoSansCJKkr",
-                    fontSize: 12,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ],
-            ),
-          ),
     );
   }
 }
