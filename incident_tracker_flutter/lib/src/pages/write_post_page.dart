706bee41033c586b0999372af68c5d55c37d7b11
sirasatarato <sirasatarato@gmail.com>
2021-05-11 16:10:12 +0900

    Create WritePostPage

---
+++ incident_tracker_flutter/lib/src/pages/write_post_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WritePostPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
